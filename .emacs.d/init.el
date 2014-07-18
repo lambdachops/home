2fda1e904c5bdea91cf7b11a7198867167770a11
Adrian Cumiskey <adrian.cumiskey@gmail.com>
2014-07-18 22:53:53 +0800

    Optionally adjust exec path if running on mac

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,11 @@
 ;;fix the PATH variable
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-       (setq exec-path (split-string path-from-shell path-separator))))
-(when window-system (set-exec-path-from-shell-PATH))
+(when (eq system-type 'darwin)
+  (progn
+    (defun set-exec-path-from-shell-PATH ()
+      (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
+      (setenv "PATH" path-from-shell)
+         (setq exec-path (split-string path-from-shell path-separator))))
+    (when window-system (set-exec-path-from-shell-PATH))))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
