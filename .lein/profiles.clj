fef9eefe4a3b3d8575e97eff83175447e9809ef5
Adrian Cumiskey <adrian.cumiskey@gmail.com>
2014-07-28 17:02:09 +0800

    Updated android profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,15 @@
 {:user {
+
   :plugins [
     [cider/cider-nrepl "0.6.0"]
     [lein-droid "0.2.3"]
   ]
-  :android-dev [
-  ]
+
+  :android {
+    :sdk-path "/home/adrian/apps/adt-bundle-linux-x86_64/sdk"
+  }
+
+  :android-dev {
+    :sdk-path "/home/adrian/apps/adt-bundle-linux-x86_64/sdk"
+  }
 }}
